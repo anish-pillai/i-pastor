3196eb674f11ce26308d60a2fd406ebfcc6283f7
Anish K Pillai <vsuser@ana-data.com>
2025-01-01 20:51:55 -0500

    Remove dist folder and add to .gitignore

--- server-node/dist/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (error, req, res, next) => {
-    console.error('Error Middleware:', error);
-    res.status(500).send('Server error');
-};
-exports.errorHandler = errorHandler;
